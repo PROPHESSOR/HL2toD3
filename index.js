a1c0b7196d6e776339bbc8d00364d62485efd484
PROPHESSOR <xenteze@gmail.com>
2018-04-05 21:30:25 +0300

    + Рабочее преобразование vmf в json + Рабочее разделение на world и entities + Не рабочие entity

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const test = {
+	get getid() {
+		return ~~(Math.random() * 10);
+	}
+}
+
+function main() {
+	const file = fs.readFileSync('./background01.vmf', 'utf8');
+
+	const outfile = file
+		// .replace(/(world)\s+/g, '"$1": ')
+		.replace(/world\s+/g, '')
+		.replace(/\n{/g, ' {')
+		.replace(/\n\s*(solid|side|connections)/g, ',\n')
+		.replace(/"\n(\s*)"/g, '",\n$1"')
+		.replace(/"\s+"/g, '": "')
+		.replace(/",\n(\s*){/g, '",\n$1"array": [\n$1\t{')
+		.replace(/}\n(\s*)}/g, '}\n$1\t]\n$1}')
+		.replace(/}\n\s*entity/, ']}\n\n================\n\nentity')
+
+	const outfile1 = outfile.split(/=+/)[0];
+	const outfile2 = "[\n" + outfile.split(/=+/)[1]
+		.replace(/entity/g, '')
+		.replace(/}\n\s*{/g, '},\n{')
+		
+		// .replace(/{\n(\s*)"OnTrigger": /g, '')
+		// .replace(/"OnTrigger": "(.+)",\n(\s*)}/g, '"$2",\n$2]\n$2}')
+		// .replace(/"OnTrigger": /g, '')
+		// .replace(/^(\s*"[\w_,\d-]+",?)\n\s*}/g, '$1\n')
+		+ "\n]"
+
+	fs.writeFileSync('./background01.json', outfile1, 'utf8');
+	fs.writeFileSync('./background01-entities.json', outfile2, 'utf8');
+}
+
+main();
